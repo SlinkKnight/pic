e3e71d8b18ef9d2dbd93835cbf77c5af7e7bafb0
vicenzoStopa <vicenzostop@outlook.com>
2026-04-03 19:19:03 -0300

    display duplo, 0-99

--- 31-03/main.c
+++ 31-03/main.c
@@ -5,17 +5,6 @@
 
 #FUSES NOPUT, NOBROWNOUT, NOLVP, HS, NOWDT
 
-#include <lcd.c> 
-
-#define LCD_RS_PIN      PIN_D7
-#define LCD_RW_PIN      PIN_D6
-#define LCD_ENABLE_PIN  PIN_D5
-#define LCD_DATA4       PIN_D4
-#define LCD_DATA5       PIN_D3
-#define LCD_DATA6       PIN_D2
-#define LCD_DATA7       PIN_D1
-
-
 #define LEDA    PIN_C0
 #define LEDB    PIN_C1
 #define LEDC    PIN_C2
@@ -24,123 +13,203 @@
 #define LEDF    PIN_C5
 #define LEDG    PIN_C6
 
-void clearDisplay(){
-    output_high(LEDA);
-    output_high(LEDB);
-    output_high(LEDC);
-    output_high(LEDD);
-    output_high(LEDE);
-    output_high(LEDF);
-    output_high(LEDG);
+#define LEDA2    PIN_D0
+#define LEDB2    PIN_D1
+#define LEDC2    PIN_D2
+#define LEDD2    PIN_D3
+#define LEDE2    PIN_D4
+#define LEDF2    PIN_D5
+#define LEDG2    PIN_D6
+
+void clearDisplay(int flag){
+
+    if(flag) {
+        output_high(LEDA);
+        output_high(LEDB);
+        output_high(LEDC);
+        output_high(LEDD);
+        output_high(LEDE);
+        output_high(LEDF);
+        output_high(LEDG);
+    } else {
+        output_high(LEDA2);
+        output_high(LEDB2);
+        output_high(LEDC2);
+        output_high(LEDD2);
+        output_high(LEDE2);
+        output_high(LEDF2);
+        output_high(LEDG2);
+    }
 }
 
-void display1() {
-    output_low(LEDB);
-    output_low(LEDC);
-}
-void display2() {
-    output_low(LEDD);
-    output_low(LEDE);
-    output_low(LEDG);
-    output_low(LEDB);
-    output_low(LEDA);
-}
-void display3() {
-    output_low(LEDA);
-    output_low(LEDB);
-    output_low(LEDG);
-    output_low(LEDC);
-    output_low(LEDD);
-}
-void display4() {
-    output_low(LEDC);
-    output_low(LEDG);
-    output_low(LEDF);
-    output_low(LEDB);
-}
-void display5() {
-    output_low(LEDA);
-    output_low(LEDF);
-    output_low(LEDG);
-    output_low(LEDC);
-    output_low(LEDD);
-}
-void display6() {
-    output_low(LEDA);
-    output_low(LEDF);
-    output_low(LEDG);
-    output_low(LEDC);
-    output_low(LEDD);
-    output_low(LEDE);
+void choose(int un, int dec) {
+    switch (un)
+    {
+    case 0:
+        output_low(LEDA);
+        output_low(LEDB);
+        output_low(LEDC);
+        output_low(LEDD);
+        output_low(LEDE);
+        output_low(LEDF);
+        break;
+    case 1:
+        output_low(LEDB);
+        output_low(LEDC);
+        break;
+    case 2:
+        output_low(LEDA);
+        output_low(LEDB);
+        output_low(LEDD);
+        output_low(LEDE);
+        output_low(LEDG);
+        break;
+    case 3:
+        output_low(LEDA);
+        output_low(LEDB);
+        output_low(LEDC);
+        output_low(LEDD);
+        output_low(LEDG);
+        break;
+    case 4:
+        output_low(LEDB);
+        output_low(LEDC);
+        output_low(LEDF);
+        output_low(LEDG);
+        break;
+    case 5:
+        output_low(LEDA);
+        output_low(LEDC);
+        output_low(LEDD);
+        output_low(LEDF);
+        output_low(LEDG);
+        break;
+    case 6:
+        output_low(LEDA);
+        output_low(LEDC);
+        output_low(LEDD);
+        output_low(LEDE);
+        output_low(LEDF);
+        output_low(LEDG);
+        break;
+    case 7:
+        output_low(LEDA);
+        output_low(LEDB);
+        output_low(LEDC);
+        break;
+    case 8:
+        output_low(LEDA);
+        output_low(LEDB);
+        output_low(LEDC);
+        output_low(LEDD);
+        output_low(LEDE);
+        output_low(LEDF);
+        output_low(LEDG);
+        break;
+    case 9:
+        output_low(LEDA);
+        output_low(LEDB);
+        output_low(LEDC);
+        output_low(LEDD);
+        output_low(LEDF);
+        output_low(LEDG);
+        break;
+    default:
+        break;
+    }
 
-}
-void display7() {
-    output_low(LEDA);
-    output_low(LEDB);
-    output_low(LEDC);
-}
-void display8() {
-    output_low(LEDA);
-    output_low(LEDF);
-    output_low(LEDG);
-    output_low(LEDC);
-    output_low(LEDD);
-    output_low(LEDE);
-    output_low(LEDB);
-}
-void display9() {
-    output_low(LEDA);
-    output_low(LEDF);
-    output_low(LEDG);
-    output_low(LEDC);
-    output_low(LEDD);
-    output_low(LEDB);
-}
-void display0() {
-    output_low(LEDF);
-    output_low(LEDE);
-    output_low(LEDD);
-    output_low(LEDC);
-    output_low(LEDB);
-    output_low(LEDA);
+    switch (dec)
+    {
+    case 0:
+        output_low(LEDA2);
+        output_low(LEDB2);
+        output_low(LEDC2);
+        output_low(LEDD2);
+        output_low(LEDE2);
+        output_low(LEDF2);
+        break;
+    case 1:
+        output_low(LEDB2);
+        output_low(LEDC2);
+        break;
+    case 2:
+        output_low(LEDA2);
+        output_low(LEDB2);
+        output_low(LEDD2);
+        output_low(LEDE2);
+        output_low(LEDG2);
+        break;
+    case 3:
+        output_low(LEDA2);
+        output_low(LEDB2);
+        output_low(LEDC2);
+        output_low(LEDD2);
+        output_low(LEDG2);
+        break;
+    case 4:
+        output_low(LEDB2);
+        output_low(LEDC2);
+        output_low(LEDF2);
+        output_low(LEDG2);
+        break;
+    case 5:
+        output_low(LEDA2);
+        output_low(LEDC2);
+        output_low(LEDD2);
+        output_low(LEDF2);
+        output_low(LEDG2);
+        break;
+    case 6:
+        output_low(LEDA2);
+        output_low(LEDC2);
+        output_low(LEDD2);
+        output_low(LEDE2);
+        output_low(LEDF2);
+        output_low(LEDG2);
+        break;
+    case 7:
+        output_low(LEDA2);
+        output_low(LEDB2);
+        output_low(LEDC2);
+        break;
+    case 8:
+        output_low(LEDA2);
+        output_low(LEDB2);
+        output_low(LEDC2);
+        output_low(LEDD2);
+        output_low(LEDE2);
+        output_low(LEDF2);
+        output_low(LEDG2);
+        break;
+    case 9:
+        output_low(LEDA2);
+        output_low(LEDB2);
+        output_low(LEDC2);
+        output_low(LEDD2);
+        output_low(LEDF2);
+        output_low(LEDG2);
+        break;
+    default:
+        break;
+    }
 }
 
-void main ()
+void main()
 {
-    lcd_init(); 
-    output_low(PIN_D0);
+    output_low(PIN_B4);
+    output_low(PIN_B5);
 
     while(1)
     {
-        clearDisplay();
-        display0();
-        delay_ms(500);
-        clearDisplay();
-        display1();
-        delay_ms(500);
-        clearDisplay();
-        display2();
-        delay_ms(500);
-        clearDisplay();
-        display3();
-        delay_ms(500);
-        clearDisplay();
-        display4();
-        delay_ms(500);
-        clearDisplay();
-        display5();
-        delay_ms(500);
-        clearDisplay();
-        display6();
-        delay_ms(500);
-        clearDisplay();
-        display7();
-        delay_ms(500);
-        clearDisplay();
-        display8();
-        delay_ms(500);
-        clearDisplay();
-        display9();
-        delay_ms(500);
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                clearDisplay(1);
+                clearDisplay(0);
+                choose(j, i);
+                delay_ms(500);
+            }
+        }
     }
 }
